e60c39be8d4aee55b09adbbe1b32d230b21313d2
Pratyush Makkar <pratyushmakkar@gmail.com>
2023-07-14 23:14:41 -0400

    finished PingPong top. Left to verify

--- src/rtl/PingPong.sv
+++ src/rtl/PingPong.sv
@@ -1,3 +1,5 @@
+`include "BallPhysics.sv"
+`include "PaddleAlgorithm.sv"
 module PingPong (
   input logic rst,
   input logic clk,
@@ -11,9 +13,44 @@ module PingPong (
   output logic [31:0] leftPaddlePositionOut,
   output logic [31:0] rightPaddlePositionOut,
   output logic [31:0] ballPositionOut,
-  output logic [15:0] ballVelocityOut
+  output logic [31:0] ballVelocityOut
 );
 
+  logic [1:0] player_score_update_reg;
+  logic [15:0] player_score_reg;
+
+  automatedPaddle PaddleAlgorithm (
+    .clk(clk),
+    .rst(rst),
+    .ballPosition(ballPosition),
+    .ballVelocity(ballVelocity),
+    .paddlePositionIn(leftPaddlePosition),
+    .paddlePositionOut(leftPaddlePosition),
+  );
+
+  ballPixel BallPhysics (
+    .rst(rst),
+    .clk(clk),
+    .dimensions(dimensions),
+    .ballPosition(ballPosition),       // X- Y value
+    .ballVelocity(ballVelocity),
+    .leftPaddlePosition(leftPaddlePosition),
+    .rightPaddlePosition(rightPaddlePosition),
+    .playerDidScore(player_score_update_reg)                    // RL
+    .ballPositionOut(ballPosition),
+    .ballVelocityOut(ballVelocity),
+  );
+
+  always_ff @(posedge clk) begin
+    if (player_score_reg != 2'b00) begin
+      if (player_score_update_reg[1]) begin
+        player_score_reg[15:8] <= player_score_reg[15:8] + 8'h01;
+      end else begin
+        player_score_reg[7:0] <= player_score_reg[7:0] + 8'h01;
+      end
+    end
+  end
   
-  
+  assign scoreOut = player_score_reg;
+
 endmodule
